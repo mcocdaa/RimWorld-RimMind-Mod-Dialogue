a5ffbc4cfff39b3c1a936f238be46d916aa6ae00
mcocdaa <2021137961@qq.com>
2026-05-17 09:01:05 +0800

    202605170858
    fix: correct namespace references and test assertions for architecture refactor
    
    - Update namespace in DialogueSettingsTab.cs to match new project structure
    - Fix corrupted Chinese characters in test assertions (ellipsis, punctuation)
    - Update project references to align with Domain-driven design

--- Source/Extensions/DialogueSettingsTab.cs
+++ Source/Extensions/DialogueSettingsTab.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using Verse;
-using RimMind.Presentation.Settings;
+using RimMind.Application.Common.Interfaces.Extension;
 using RimMind.Dialogue.Settings;
 
 namespace RimMind.Dialogue
